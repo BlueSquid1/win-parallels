fce3b8478f00767763f5a2d859522712ae470371
Clinton Page <clinton.page123@gmail.com>
2024-03-17 14:59:20 +1030

    polishing parallels vm

--- Vagrantfile
+++ Vagrantfile
@@ -6,11 +6,12 @@ $script = File.read(__dir__ + "/rebuild.ps1")
 Vagrant.configure("2") do |config|
   config.vm.define "win-2022"
   config.vm.box = "stromweld/windows-2022"
-  config.vm.provider "parallels"
-  config.vm.boot_timeout = 120
+  config.vm.boot_timeout = 150
   config.vm.communicator = "winrm"
   
-  config.vm.network "forwarded_port", guest: 22, host: 22
   config.vm.network "private_network", ip: "192.168.50.2"
-  config.vm.provision "shell", inline: $script
+
+  config.vm.synced_folder "/tmp/shared", "C:\\windows\\Temp\\shared", create: true
+
+  config.vm.provision "shell", inline: $script  
 end
--- rebuild.ps1
+++ rebuild.ps1
@@ -9,18 +9,31 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-
 
 echo "install choco"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Create a powershell profile
 New-Item -path $PROFILE -type file -force
 Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\ProgramData\chocolatey\bin'"
 
 echo "install dev tools"
 choco install -y git
+Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\Program Files\Git\bin'"
 choco install -y netcat
-#choco install -y visualstudio2022buildtools
-#Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin'"
-#Export-ModuleMember -Alias refreshenv -Function 'Update-SessionEnvironment', 'TabExpansion'
-#Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
-#refreshenv
+choco install -y visualstudio2022buildtools
+Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin'"
+choco install -y firefox
 
 #ROSE
+#cd ~/Desktop
+#reload terminal
+#git clone https://github.com/BlueSquid1/rose.git
+#cd rose
+#& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe' modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" --config ".\client\rose.vsconfig" --quiet
+#Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\Program Files\dotnet'"
+#cd client
+#reload terminal
+#dotnet restore
+#MSBuild.exe /property:Configuration=Release
+#cp -r .\rose\bin\Release\net8.0 'C:\Program Files\Rose'
 #Add-Content -Path $PROFILE -Value "`$env:Path += ';C:\Program Files\Rose'"
 #reload terminal
+#open regedit and set SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\fAllowUnlistedRemotePrograms to 1
