9a917280d074251b22f305061be0f9272a368277
Clinton Page <clintonpage@Clintons-MBP.lan>
2024-07-06 20:11:53 +0930

    now with some choco packages

--- src/vagrant/destroy_container.sh
+++ src/vagrant/destroy_container.sh
@@ -4,6 +4,4 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export VAGRANT_CWD="$SCRIPT_DIR"
 
-if vagrant box list | grep -q "win-11"; then
-    BOX_PATH="${1}" VM_NAME="${2}" vagrant box remove "${1}" --force --all
-fi
+BOX_PATH="${1}" VM_NAME="${2}" vagrant destroy "${2}" --force
